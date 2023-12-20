09460c943b4426b34ff6a465e1c1f8c151fbd1c5
Ray Zana <rayzana280@gmail.com>
2023-12-20 14:50:15 -0500

    creating a table that would collect datdt for biscep workout, adding rows for workout their sets and reps

--- db/migrate/20231220193304_create_biceps_workouts.rb
+++ db/migrate/20231220193304_create_biceps_workouts.rb
@@ -1,4 +1,9 @@
 class CreateBicepsWorkouts < ActiveRecord::Migration[6.1]
   def change
+    create_table :biceps_workouts do |t|
+      t.string :name 
+      t.integer :sets
+      t.integer :reps 
+    end 
   end
 end
