8793186d6b198b303cda665df4b9afc00c65e8f5
Ray Zana <rayzana280@gmail.com>
2023-12-24 00:21:48 -0500

    adding data for our app to seed to have something to work with

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+puts "seeding"
+Bicepworkout.create(name: "Hammer curl", sets: 3, reps: 10)
+Bicepworkout.create(name: "Bicep curl", sets: 3, reps: 10)
+Bicepworkout.create(name: "Spider curl", sets: 3, reps: 10)
+puts "done seeding"
