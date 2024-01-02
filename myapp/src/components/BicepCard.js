f4dce76828e49dd42c3121080de3e95d78f67e99
Ray Zana <rayzana280@gmail.com>
2024-01-02 13:00:07 -0500

    adding a button to display people experience on the workout

--- myapp/src/components/BicepCard.js
+++ myapp/src/components/BicepCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function BicepCard({item}){
+    function handleclick(){
+        console.log("this button been click")
+    }
     return(
         <div>
             <h2>WORKOUT: {item.name}</h2>
             <h2>SETS: {item.sets}</h2>
             <h2>REPS: {item.reps}</h2>
+            <button onClick={handleclick}>Show Comments</button>
         </div>
     )
 };
