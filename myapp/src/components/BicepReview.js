def7242398721cf60a7c088402f38711a98aabfe
Ray Zana <rayzana280@gmail.com>
2024-01-02 13:02:20 -0500

    adding a form for user to add ther comments and experience about the workou, will using this form for other workout

--- myapp/src/components/BicepReview.js
+++ myapp/src/components/BicepReview.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function BicepReview (){
-
+    return(
+        <form>
+            <label>ADD YOUR EXPERIENCE:</label>
+            <input/>
+        </form>
+    )
 };
 
 export default BicepReview;
