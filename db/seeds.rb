27448ab12a6a49d70b325e0f5e2925b038e3554d
Ray Zana <rayzana280@gmail.com>
2024-01-03 22:31:45 -0500

    creating some new objects for review

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,7 @@ puts "seeding"
 Bicepworkout.create(name: "Hammer curl", sets: 3, reps: 10)
 Bicepworkout.create(name: "Bicep curl", sets: 3, reps: 10)
 Bicepworkout.create(name: "Spider curl", sets: 3, reps: 10)
+Review.create(comment: "if you are new to working out try hammer curl witj 7 reps", workout_id: 1)
+Review.create(comment: "Bicep curl are really good on the bicep try to set the reps to 15", workout_id: 2)
+Review.create(comment: "Spider curl is a little advance for people but good to learn", workout_id: 3)
 puts "done seeding"
