a0fe78f40a11120fc9e1c2be46c9002a14cd6237
Ray Zana <rayzana280@gmail.com>
2024-01-07 20:23:23 -0500

    adding another useEffect to fetch the review data to display

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,16 +6,25 @@ import Biceps from './components/Biceps';
 function App() {
 
   const[bicepsData, stateBicepsData] = useState([])
+  const [reviewData, stateReviewData] = useState([])
 
   useEffect(()=>{
     fetch("http://localhost:9292/")
     .then((res) => res.json())
     .then((data) => stateBicepsData(data))
   }, [])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/review")
+    .then((res) => res.json())
+    .then((data)=> stateReviewData(data))
+  }, [])
+
+
   
   return (
     <div className="App">
-      <Biceps data={bicepsData}/>
+      <Biceps data={bicepsData} reviewD={reviewData}/>
     </div>
   );
 }
