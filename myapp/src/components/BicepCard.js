732c4261af7e55dda7f3fdd743f20a772c247032
Ray Zana <rayzana280@gmail.com>
2024-01-17 22:56:08 -0500

    adding a function update the state array when comment is being delete from datatbase

--- myapp/src/components/BicepCard.js
+++ myapp/src/components/BicepCard.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import BicepReview from "./BicepReview";
+import Comments from "./Comments";
 
 function BicepCard({item, reviewD}){
 
+    const [button, stateButton]= useState(false)
+    const [newarray, stateNewArray] = useState(reviewD)
+
+
     function handleclick(){
-        console.log("this button been click")
-        console.log(reviewD) //this review data just need to display them 
+        stateButton(!button)
+        const array = reviewD.filter((element)=> element.workout_id === item.id)
+        stateNewArray(array)
+        console.log(item.id)
+        //this review data just need to display them 
+    }
+    
+    function addData(newData){
+        stateNewArray([...newarray, newData])
+    }
+
+    function updateData(deleteData){
+        const newArrays= newarray.filter((item)=>{
+           return item.id !== deleteData.id
+        })
+        stateNewArray(newArrays)
     }
 
     return(
@@ -13,8 +32,11 @@ function BicepCard({item, reviewD}){
             <h2>WORKOUT: {item.name}</h2>
             <h2>SETS: {item.sets}</h2>
             <h2>REPS: {item.reps}</h2>
-            <BicepReview itemId={item.id}/>
-            <button onClick={handleclick}>Show Comments</button>
+            <BicepReview itemId={item.id} addData={addData}/>
+            <button onClick={handleclick}>{button ? "Show less Comments" : "Show Comments" }</button>
+            {button ? newarray.map((item)=>{
+                return <Comments items={item} key={item.id} updateData={updateData}/>
+            }) : null }
         </div>
     )
 };
