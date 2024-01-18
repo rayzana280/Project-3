2b7c5ef27860130ad60e1176f1aa90ad90c444a8
Ray Zana <rayzana280@gmail.com>
2024-01-17 23:00:19 -0500

    adding handling delete request for tricep and bicep comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,34 @@ class ApplicationController < Sinatra::Base
         )
         review.to_json
     end 
+
+    get '/triceps' do 
+        tricpeworkout = Tricepworkout.all
+        tricpeworkout.to_json
+    end 
+
+    get '/tricepsreview' do 
+        review = Tricepreview.all
+        review.to_json
+    end 
+
+    post '/tricepsreview' do 
+        review = Tricepreview.create(
+            comment: params[:comment],
+            tricepworkout_id: params[:tricepworkout_id]
+        )
+        review.to_json
+    end 
+
+    delete '/review/:id' do 
+        review = Review.find(params[:id])
+        review.destroy
+        review.to_json
+    end 
+
+    delete '/tricepsreview/:id' do
+        review = Tricepreview.find(params[:id])
+        review.destroy
+        review.to_json 
+    end 
 end 
