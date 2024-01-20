44e1309ae67e6f898ddcb1bf3056f20654b18ce1
Ray Zana <rayzana280@gmail.com>
2024-01-19 23:14:35 -0500

    adding home components to explain what the app is about

---
+++ myapp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Home(){
+    return(
+        <div>
+            <h3>WELCOME TO WORKOUT ROUTINE</h3>
+            <p>Ready to get your mind and body right, This app is going to help with everything you need!</p>
+            <p>
+                This workout app would give workouts, plus guide you on reps and sets to do see help improve staying fit and building the dream body
+            </p>
+        </div>
+    )
+}
+
+export default Home;
