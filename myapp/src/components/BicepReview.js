045eac34bdfdb65fe6fbda3434c07f087fafba34
Ray Zana <rayzana280@gmail.com>
2024-01-25 00:25:37 -0500

    making changes to make this components more reuseable

--- myapp/src/components/BicepReview.js
+++ myapp/src/components/BicepReview.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-function BicepReview (){
+function BicepReview({itemId, addData, submit}){
+
+    const [comments, setComment] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault(); 
+
+        const formData = {
+            comment: comments,
+            workout_id: itemId
+        }
+        //this is where the review is going to get submit
+        submit(formData)
+        setComment("")
+    }
+
+    
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>ADD YOUR EXPERIENCE:</label>
-            <input/>
+            <input placeholder="type comment" value={comments} onChange={(e)=> setComment(e.target.value)} className="submitInput"/>
+            <button className="submitButton">SUBMIT</button>
         </form>
     )
 };
